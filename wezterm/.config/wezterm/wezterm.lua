0d1f49b5ca11a158a28224dbf3f013425dbe8e50
Almujeer Uddin <68914647+JamRuinedDuel@users.noreply.github.com>
2025-09-22 00:25:55 +0900

    add wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,60 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- Font Settings
+config.font = wezterm.font({
+  family = "MesloLGSDZ Nerd Font",
+  weight = "Regular",
+  italic = false,
+})
+config.font_size = 16
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.text_background_opacity = 0.9
+
+-- Color & Theme Settings
+config.colors = {
+  foreground = "#e5e9f0",
+  background = "#191d24",
+  ansi = {
+    "#1e222a", -- black
+    "#bf616a", -- maroon
+    "#a3be8c", -- green
+    "#ebcb8b", -- olive
+    "#5e81ac", -- navy
+    "#b48ead", -- purple
+    "#8fbcbb", -- teal
+    "#bbc3d4", -- silver
+  },
+  brights = {
+    "#434c5e", -- grey
+    "#c5727a", -- red
+    "#b1c89d", -- lime
+    "#efd49f", -- yellow
+    "#81a1c1", -- blue
+    "#be9db8", -- fuchsia
+    "#9fc6c5", -- aqua
+    "#eceff4", -- white
+  },
+}
+config.colors.selection_fg = config.colors.background
+config.colors.selection_bg = config.colors.foreground
+config.colors.cursor_bg = config.colors.foreground
+config.colors.cursor_fg = config.colors.background
+
+-- Tab Settings
+config.enable_tab_bar = false
+
+-- Window Settings
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 20
+
+-- Startup Settings
+config.default_prog = {
+  "/bin/zsh",
+  "-l",
+  "-c",
+  "tmux attach || tmux new -s Session",
+}
+
+return config
